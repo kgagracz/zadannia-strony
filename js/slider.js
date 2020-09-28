a73ee4f7108de09a49bb7c210319ebc08e92fc82
Krystian Gagracz <krystian.gagracz@gmail.com>
2020-09-28 20:57:16 +0200

    slider imgs

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,28 @@
 let images = [];
 let i = 0;
-let time = 2000;
+let time = 1000;
 
-images[0] = '../img/slajd1.jpg';
-images[1] = '../img/slajd2.jpg';
-images[2] = '../img/slajd3.jpg';
-images[3] = '../img/slajd4.jpg';
+let button1s = document.getElementById('button1');
+let button2s = document.getElementById('button2');
+let button3s = document.getElementById('button3');
+
+images[0] = 'img/slajd1.jpg';
+images[1] = 'img/slajd2.jpg';
+images[2] = 'img/slajd3.jpg';
+images[3] = 'img/slajd4.jpg';
+
+button1s.addEventListener('click', function() {
+    time = 1000
+})
+button2s.addEventListener('click', function() {
+    time = 2000
+})
+button3s.addEventListener('click', function() {
+    time = 3000
+})
 
 function swipe() {
-    document.getElementByID('slide').src = images[i];
+    document.getElementById('slide').src = images[i];
 
     if (i < images.length - 1) {
         i++;
@@ -17,5 +31,7 @@ function swipe() {
         i = 0;
     }
 
+    setTimeout(swipe, time);
 }
-setTimeout(swipe, time);
+
+window.onload = swipe;
